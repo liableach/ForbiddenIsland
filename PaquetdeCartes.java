0984c4ba22dcc97abfa0c6829fa6aa9b573e2f28
illia.vlasenko <illia.vlasenko@universite-paris-saclay.fr>
2025-04-23 18:57:50 +0200

    NTM

--- PaquetdeCartes.java
+++ PaquetdeCartes.java
@@ -6,7 +6,22 @@ public class PaquetdeCartes{
     private ArrayList<Carte> defausse;
     public PaquetdeCartes(){
         tresor = new ArrayList<Carte>();
+        for(int i = 0; i < 5; i++){
+            tresor.add(new Carte(TypeCarte.tresor_feu));
+            tresor.add(new Carte(TypeCarte.tresor_terre));
+            tresor.add(new Carte(TypeCarte.tresor_eau));
+            tresor.add(new Carte(TypeCarte.tresor_air));
+        }
+        for(int i = 0; i < 3; i++){
+            tresor.add(new Carte(TypeCarte.montee_des_eaux));
+            tresor.add(new Carte(TypeCarte.helicoptere));
+        }
+        for(int i = 0; i < 2; i++) tresor.add(new Carte(TypeCarte.sacs_de_sable));
+
+
         inondations = new ArrayList<Carte>();
+        for(int i = 0; i < 24; i++) inondations.add(new Carte(TypeCarte.inondation, i));
+
         defausse = new ArrayList<Carte>();
     }
 
