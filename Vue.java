937b981b303ea7b6768d9a5ed00f033ccf9b4a47
illia.vlasenko <illia.vlasenko@universite-paris-saclay.fr>
2025-04-24 20:58:52 +0200

    ss

--- Vue.java
+++ Vue.java
@@ -16,7 +16,7 @@ class Vue extends JPanel {
         this.ile = ile;
         setPreferredSize(new Dimension(Width, Height)); // 100px par case
     }
-
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -24,8 +24,8 @@ class Vue extends JPanel {
             for (int x = 0; x < 6; x++){
                 Zone z = ile.getZone(x, y);
                 if (z == null) continue;
+                //if(z.getType() == Type.vide) continue;
                 switch (z.getType()) {
-                    case vide -> g.setColor(Color.WHITE);
                     case normale -> g.setColor(Color.LIGHT_GRAY);
                     case heliport -> g.setColor(Color.YELLOW);
                     case element_a -> g.setColor(Color.CYAN);
@@ -33,19 +33,12 @@ class Vue extends JPanel {
                     case element_e -> g.setColor(Color.BLUE);
                     case element_f -> g.setColor(Color.RED);
                 }
-                switch (z.getEtat()){
-                    case inondee -> g.setColor(Color.CYAN);
-                    case submergee -> g.setColor(Color.WHITE);
-                }
+                if(z.getEtat() == Etat.inondee) g.setColor(Color.CYAN);
                 g.fillRect(x * 100 + Width/5, y * 100 + Height/5, 100, 100);
                 g.setColor(Color.BLACK);
                 g.drawRect(x * 100 + Width/5, y * 100 + Width/5, 100, 100);
 
                 //affichage des artefacts en sur les 4 coins de l'île
-                g.drawImage(Calice_de_l_onde, Width/5, Height/5, 100, 100, null);
-                g.drawImage(Pierre_sacree, Width/5 , Height/5 + 100*5, 100, 100, null);
-                g.drawImage(Cristal_ardent, Width/5 + 100 * 5, Height/5, 100, 100, null);
-                g.drawImage(Statue_du_Zephir, Width/5 + 100 * 5, Height/5 + 100*5, 100, 100, null);
 
                 g.drawString(z.getType().toString(), x * 100 + 10 + Width/5, y * 100 + 20 + Width/5);
             }
