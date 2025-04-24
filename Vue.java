42b547658e9cfc4b5114b2af02b55826823b32f3
mamisoa.rakotosamimanana <mamisoa.rakotosamimanana@universite-paris-saclay.fr>
2025-04-24 19:58:30 +0200

    j'ai rajouté les artefacts

--- Vue.java
+++ Vue.java
@@ -3,8 +3,14 @@ import java.awt.*;
 
 class Vue extends JPanel {
     private Ile ile;
-    int Width = 900;
-    int Height = 900;
+    public int Width = 900;
+    public int Height = 900;
+    private final Image Calice_de_l_onde = new ImageIcon(getClass().getResource("Calice_de_l_onde.png")).getImage();
+    private final Image Cristal_ardent = new ImageIcon(getClass().getResource("Cristal_ardent.png")).getImage();
+    private final Image Pierre_sacree = new ImageIcon(getClass().getResource("Pierre_sacree.png")).getImage();
+    private final Image Statue_du_Zephir = new ImageIcon(getClass().getResource("Statue_du_Zephir.png")).getImage();
+
+
     public Vue(Ile ile) {
         this.ile = ile;
         setPreferredSize(new Dimension(Width, Height)); // 100px par case
@@ -33,6 +39,12 @@ class Vue extends JPanel {
                 g.fillRect(x * 100 + Width/5, y * 100 + Height/5, 100, 100);
                 g.setColor(Color.BLACK);
                 g.drawRect(x * 100 + Width/5, y * 100 + Width/5, 100, 100);
+
+                //affichage des artefacts en sur les 4 coins de l'îles
+                g.drawImage(Calice_de_l_onde, Width/5, Height/5, 100, 100, null);
+                g.drawImage(Pierre_sacree, Width/5 , Height/5 + 100*5, 100, 100, null);
+                g.drawImage(Cristal_ardent, Width/5 + 100 * 5, Height/5, 100, 100, null);
+                g.drawImage(Statue_du_Zephir, Width/5 + 100 * 5, Height/5 + 100*5, 100, 100, null);
 
                 g.drawString(z.getType().toString(), x * 100 + 10 + Width/5, y * 100 + 20 + Width/5);
             }
